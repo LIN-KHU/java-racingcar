301cb8a16a776d95543bdd3349fadf651a5d1185
bjkim <bjkim3059@gmail.com>
2024-01-06 10:57:38 +0900

    {자동차명 입력 메소드화, 시도할 횟수 입력 완성}

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -21,19 +21,14 @@ public class Application {
         }
     }
 
-    public static void main(String[] args) {
-        // TODO 구현 진행
-        // 1. 자동차명 입력
+    static String[] inputCarName() {
         boolean bool = false;
         String[] carNameList;
-
         do {
             bool = false;
             System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
             String carNameStr = Console.readLine();
-
             carNameList = carNameStr.split(",");
-
             for (int i = 0; i< carNameList.length; i++) {
                 String carName = carNameList[i].trim();
                 carNameList[i] = carName;
@@ -42,6 +37,30 @@ public class Application {
                 }
             }
         } while (bool);
+        return carNameList;
+    }
+
+    static int inputAttemptCount() {
+        int num;
+        while (true) {
+            System.out.println("시도할 회수는 몇회인가요?");
+            String numStr = Console.readLine();
+            try {
+                num = Integer.parseInt(numStr);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] 시도 횟수는 숫자여야 한다.");
+            }
+        }
+        return num;
+    }
+
+    public static void main(String[] args) {
+        // TODO 구현 진행
+        // 1. 자동차명 입력
+        String[] carNameList = inputCarName();
+        int num = inputAttemptCount();
+
 
     }
 }
