8efe77240f13a412f4c95b3a0bbcf4fcce6ad354
Hyoseop Song <crad_on25@naver.com>
2024-01-09 02:06:55 +0900

    feat: Car 객체 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,30 @@
 package racingcar;
 
 public class Car {
+
+    private static final int MAX_NAME_LENGTH = 5;
+    private static final int MOVE_CONDITION = 4;
     private final String name;
     private int position = 0;
 
     public Car(String name) {
+        if (name == null || name.trim().isEmpty() || name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름은 1~5자 이내만 가능합니다.");
+        }
         this.name = name;
     }
 
-    // 추가 기능 구현
+    public void move(int number) {
+        if (number >= MOVE_CONDITION) {
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
