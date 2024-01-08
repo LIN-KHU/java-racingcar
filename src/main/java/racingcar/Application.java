5f5d000bb21039385b21639f6d7a9884c8efddca
Co-DDING <90135669+NoMyeongEun@users.noreply.github.com>
2024-01-09 05:04:49 +0900

    { 1. 자동차 목록(이름)과 시도 횟수 입력받기 }

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,44 @@
 package racingcar;
+import java.util.*;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+    public static void InputError(String[] arr) {
+        for (String str : arr) {
+            if (str.isEmpty() || (str.length() >= 6)) {
+                throw new IllegalArgumentException("[ERROR] 자동차 이름이 비어있거나 6자 이상임");
+            }
+        }
+    }
+    public static String[] InputName() {
+        while (true) {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            String carname = Console.readLine();
+            String[] returnArr = carname.split(",");
+            try {
+                InputError(returnArr);
+                return returnArr;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static List<Car> MakeCar() {
+        String[] carnameArray = InputName();
+        List<Car> cars = new ArrayList<>();
+        for (int i = 0; i < carnameArray.length; i++) {
+            cars.add(new Car(carnameArray[i]));
+        }
+        return cars;
+    }
+
+    public static int InputCnt() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return Integer.parseInt(Console.readLine());
+    }
     public static void main(String[] args) {
-        // TODO 구현 진행
+        List<Car> cars = MakeCar();
+        int trycount = InputCnt();
     }
 }
