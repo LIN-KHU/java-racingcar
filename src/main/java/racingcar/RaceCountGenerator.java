81c1bdeaa86fa94d0356c2cecf71ab8cfab9908e
Hyoseop Song <crad_on25@naver.com>
2024-01-09 02:40:07 +0900

    feat: 랜덤 숫자를 생성하는 클래스
    - Car 객체 내에서 호출할 이유가 없다고 생각하여 분리하였으나, 다른 객체에서 이를 주입하거나 테스트할 때의 명확한 기준이 필요하다.

---
+++ src/main/java/racingcar/RaceCountGenerator.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RaceCountGenerator {
+
+    public int makeRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+}
