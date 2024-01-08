1ca7a1415773c4b9ef0ebebef76d262f115b00a8
Hyoseop Song <crad_on25@naver.com>
2024-01-09 02:37:08 +0900

    feat: 자동차의 상태를 관리하는 Cars 컬렉션 구현
    - 경주가 시작하는 즉시 모든 자동차는 불변성을 가진다.
    - 일급 컬렉션을 정의하여 경주의 진행에 있어 외부의 변경을 방지한다.

---
+++ src/main/java/racingcar/Cars.java
@@ -0,0 +1,34 @@
+package racingcar;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Cars {
+    private final List<Car> cars;
+    private final RaceCountGenerator raceCountGenerator;
+
+    public Cars(final String carNames, final RaceCountGenerator raceCountGenerator) {
+        this.cars = create(carNames);
+        this.raceCountGenerator = raceCountGenerator;
+    }
+
+    public static Cars create(String carNames, RaceCountGenerator raceCountGenerator) {
+        return new Cars(carNames, raceCountGenerator);
+    }
+
+    public void startRace() {
+        cars.forEach(car -> car.move(raceCountGenerator.makeRandomNumber()));
+    }
+
+    private List<Car> create(String carNames) {
+        String[] names = carNames.split(",");
+        return Arrays.stream(names)
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+}
