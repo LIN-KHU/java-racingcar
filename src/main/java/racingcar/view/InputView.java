82301cdae90cd6509f753589a48cfc2156b01d6e
Hyoseop Song <crad_on25@naver.com>
2024-01-09 16:29:43 +0900

    fix: 잘못된 입력 시 응답 대기를 반복하도록 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,18 +7,36 @@ import racingcar.util.InputValidator;
 
 public class InputView {
     public static List<String> getCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String input = Console.readLine();
-        List<String> carNames = Arrays.asList(input.split(","));
-        InputValidator.validateCarNames(carNames);
+        List<String> carNames = null;
+        boolean isValid = false;
+        while (!isValid) {
+            try {
+                System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+                String input = Console.readLine();
+                carNames = Arrays.asList(input.split(","));
+                InputValidator.validateCarNames(carNames);
+                isValid = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] " + e.getMessage());
+            }
+        }
         return carNames;
     }
 
     public static int getRounds() {
-        System.out.println("시도할 회수는 몇회인가요?");
-        String input = Console.readLine();
-        InputValidator.verifyRounds(input);
-
-        return Integer.parseInt(input);
+        int rounds = 0;
+        boolean isValid = false;
+        while (!isValid) {
+            try {
+                System.out.println("시도할 회수는 몇회인가요?");
+                String input = Console.readLine();
+                InputValidator.verifyRounds(input);
+                rounds = Integer.parseInt(input);
+                isValid = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] " + e.getMessage());
+            }
+        }
+        return rounds;
     }
 }
