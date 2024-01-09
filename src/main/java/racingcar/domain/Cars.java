a27ee40ba23f07fbf192df7e1290a0d67faa2e27
Hyoseop Song <crad_on25@naver.com>
2024-01-09 17:59:20 +0900

    refactor: Car 컬렉션이 생성될 때 중복을 판단하도록 검증 로직 구현

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -2,6 +2,7 @@ package racingcar.domain;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 import racingcar.util.RaceCountGenerator;
@@ -13,6 +14,7 @@ public class Cars {
     public Cars(final String carNames, final RaceCountGenerator raceCountGenerator) {
         this.cars = create(carNames);
         this.raceCountGenerator = raceCountGenerator;
+        validateDuplicateCarName();
     }
 
     public static Cars create(String carNames, RaceCountGenerator raceCountGenerator) {
@@ -28,6 +30,13 @@ public class Cars {
         return Arrays.stream(names)
                 .map(Car::new)
                 .collect(Collectors.toList());
+    }
+
+    private void validateDuplicateCarName() {
+        int uniqueCarCount = new HashSet<>(cars).size();
+        if (cars.size() != uniqueCarCount) {
+            throw new IllegalArgumentException("중복된 차량 이름이 있습니다.");
+        }
     }
 
     public List<Car> getCars() {
