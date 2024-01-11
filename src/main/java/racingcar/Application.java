24e2faaa5d11a0f083c2a4fd2f55dc8531f27a81
Jihyeon An <jh011007@naver.com>
2024-01-12 00:00:46 +0900

    자동차 이름 입력 받음

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
+
+        //차 이름 입력 받음
+        Car[] carArray = getCarName();
+
+    private static Car[] getCarName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String cars = Console.readLine(); //String
+        return setCars(cars);
+    }
     }
 }
