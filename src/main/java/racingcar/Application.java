b9b33726bf48663ef0f7c57d5ecee6d5ed0b1274
SOOsuhyuncho <chosh0132@khu.ac.kr>
2024-01-13 21:27:25 +0900

    Put all the things on the Application, ~ Print without format

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,13 +1,60 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 
 public class Application {
     public static void main(String[] args) {
-        // StartGame 클래스의 startGame 메서드 직접 호출
-        StartGame.startGame();
 
-        // EndGame 클래스의 gameResult 메서드 호출
-        EndGame.gameResult();
+        String[] carNames = getNames();
+        int tries = getTries();
+        ArrayList<Car> Cars = new ArrayList<>();
+        for (String car : carNames) {
+            Car c = new Car(car);
+            Cars.add(c);
+        }
+        for (int i = 0; i <= tries; i++) {
+            for (Car car : Cars) {
+                int number = randomNum();
+                if (number >= 4) {
+                    car.move();
+                }
+            }
+        }
+            int maxPosition = 0;
+            ArrayList<String> Winners = new ArrayList<>();
+            for (Car car : Cars) {
+                if (car.getPosition() >= maxPosition) {
+                    maxPosition = car.getPosition();
+                    Winners.add(car.getName());
+                }
+            }
+            System.out.println(Winners);
     }
-}
+
+
+
+            public static int randomNum ()
+            {
+                return Randoms.pickNumberInRange(1, 9);
+            }
+
+
+            public static String[] getNames ()
+            {
+                System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+                String cars = Console.readLine();
+                return cars.split(",");
+            }
+            public static int getTries ()
+            {
+                System.out.println("시도할 회수는 몇회인가요?");
+                String tries = Console.readLine();
+                int T = Integer.parseInt(tries);
+                return T;
+            }
+
+
+        }
