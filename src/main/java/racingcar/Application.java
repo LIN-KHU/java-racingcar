cdc725c7dbfe71718c1d3a0697c1f33132637453
bjkim <bjkim3059@gmail.com>
2024-01-15 23:15:17 +0900

    {4차 수정}

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,36 +2,61 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
+import org.mockito.internal.matchers.Null;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /*
 메소드 상세
-1. 자동차명 입력 및 오류 체크
-2. 자동차 객체 생성
-3. 시도할 횟수 입력
-4. 경주 실행
-5. 결과 출력
+1. 자동차명 입력 받기 inputCarName
+  1-1. 자동차명 예외 체크 checkCarName
+2. 자동차 객체 생성 makeCars
+3. 시도할 횟수 입력 받기 inputAttemptCount
+  3-1. 시도할 횟수 예외 체크 checkAttemptCount
+4. 경주 실행 결과 raceStartAndPrintResult
+5. 우승자 getWinner
 */
 
 public class Application {
 
-    private static String[] inputCarName() {
-        while (true) {
-            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-            String carNameStr = Console.readLine();
-            String[] carNameList = carNameStr.split(",");
-            try {
-                checkCarName(carNameList);
-                return carNameList;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+    public static void main(String[] args) {
+        // TODO 구현 진행
+        // 1. 자동차명 입력
+        String[] carNameList = inputCarName();
+
+        // 2. 자동차 객체 생성
+        List<Car> carList = makeCars(carNameList);
+
+        // 3. 시도할 수 입력
+        int num = inputAttemptCount();
+
+        // 4. 경주 실행 결과
+        raceStartAndPrintResult(num, carList);
+
+        // 5. 우승자 출력
+        System.out.println(getWinner(carList));
     }
 
-    private static void checkCarName(String[] carNameList) {
+    private static String input(String prompt) {
+        System.out.println(prompt);
+        return Console.readLine();
+    }
+
+    private static boolean catchException(Runnable callback) {
+         try {
+            callback.run();
+            return false;
+         } catch (IllegalArgumentException e) {
+             System.out.println(e.getMessage());
+             return true;
+         }
+    }
+
+    //============================================================================================================
+
+    private static void checkCarName(String carNameStr) {
+        String[] carNameList = carNameStr.split(",");
         for (String carName:carNameList) {
             carName = carName.trim();
             if (carName.length() > 5) {
@@ -40,36 +65,54 @@ public class Application {
         }
     }
 
-    private static List<Car> makeCars() {
+    private static String[] inputCarName() {
+        String carNameStr = input("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        if (catchException(() -> checkCarName(carNameStr))) {
+            inputCarName();
+        }
+        return carNameStr.split(",");
+    }
+
+    //============================================================================================================
+
+    private static List<Car> makeCars(String[] carNameList) {
         List<Car> carList = new ArrayList<>();
-        String[] carNameList = inputCarName();
         for (String carName:carNameList) {
             carList.add(new Car(carName.trim()));
         }
         return carList;
     }
 
-    private static int inputAttemptCount() {
-        int num = 0;
-        System.out.println("시도할 회수는 몇회인가요?");
-        String numStr = Console.readLine();
+    //============================================================================================================
+
+    private static void checkAttemptCount(String numStr) {
         try {
-            num = Integer.parseInt(numStr);
-        } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR] 시도 횟수는 숫자여야 한다.");
-            inputAttemptCount();
+            int num = Integer.parseInt(numStr);
+            if (num < 0) {
+                throw new IllegalArgumentException("[ERROR] 숫자는 0이상");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력하세요.");
         }
-        return num;
     }
 
-    private static void raceStart(int num, List<Car> carList) {
+    private static int inputAttemptCount() {
+        String numStr = input("시도할 회수는 몇회인가요?");
+        if (catchException(() -> checkAttemptCount(numStr))) {
+            inputAttemptCount();
+        }
+        return Integer.parseInt(numStr);
+    }
+
+    //============================================================================================================
+
+    private static void raceStartAndPrintResult(int num, List<Car> carList) {
         System.out.println("\n실행 결과");
         for (int i = 0; i < num; i++) {
-            for (int j = 0; j < carList.size(); j++) {
-                int ranNum = Randoms.pickNumberInRange(0, 9);
-                carList.get(j).move(ranNum);
-                String bar = makeBar(carList.get(j).getPosition());
-                System.out.printf("%s : %s\n", carList.get(j).getName(), bar);
+            for (Car car : carList) {
+                car.move(Randoms.pickNumberInRange(1, 9));
+                String bar = makeBar(car.getPosition());
+                System.out.printf("%s : %s\n", car.getName(), bar);
             }
             System.out.println();
         }
@@ -83,45 +126,20 @@ public class Application {
         return bar;
     }
 
-    private static List<String> getWinnerList(List<Car> carList) {
+    private static String getWinner(List<Car> carList) {
         int maxNum = 0;
-        List<String> winner = new ArrayList<String>();
-        for (int i = 0; i < carList.size(); i++) {
-            Car car = carList.get(i);
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Car car : carList) {
             if (car.getPosition() == maxNum) {
-                winner.add(car.getName());
+                stringBuilder.append(", ");
+                stringBuilder.append(car.getName());
             }
             if (car.getPosition() > maxNum) {
                 maxNum = car.getPosition();
-                winner.clear();
-                winner.add(car.getName());
+                stringBuilder.setLength(0);
+                stringBuilder.append(car.getName());
             }
         }
-        return winner;
-    }
-
-    private static void printWinner(List<String> winnerList) {
-        String winner = "";
-        for (int i = 0; i < winnerList.size(); i++) {
-            winner += winnerList.get(i);
-            if (i < winnerList.size() - 1) {
-                winner += ", ";
-            }
-        }
-        System.out.println("최종우승자 : " + winner);
-    }
-
-    public static void main(String[] args) {
-        // TODO 구현 진행
-        // 1. 자동차 객체 생성
-        List<Car> carList = makeCars();
-
-        // 2. 시도할 수 입력
-        int num = inputAttemptCount();
-
-        // 3. 실행 결과
-        raceStart(num, carList);
-
-        printWinner(getWinnerList(carList));
+        return stringBuilder.toString();
     }
 }
