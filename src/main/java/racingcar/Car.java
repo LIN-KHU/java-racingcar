3e0959bdba0a4f4e67e4500dfa48426f9eedba17
Suhyun Cho <122531178+SOOsuhyuncho@users.noreply.github.com>
2024-01-16 13:35:38 +0900

    [자동차 경주 미션 - 1단계] 조수현 미션 제출합니다. (#11)

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import java.util.ArrayList;
+
 public class Car {
     private final String name;
     private int position = 0;
@@ -8,5 +10,26 @@ public class Car {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    public void move() {
+        position++;
+    }
+
+    public String printPosition() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < this.position; i++) {
+            stringBuilder.append('-');
+        }
+        return stringBuilder.toString();
+    }
+
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
+
---
+++ src/main/java/racingcar/RandomNum.java
@@ -0,0 +1,9 @@
+package racingcar;
+import camp.nextstep.edu.missionutils.Randoms;
+public class RandomNum {
+    public static int randomNum()
+    {
+        return Randoms.pickNumberInRange(1, 9);
+    }
+
+}
